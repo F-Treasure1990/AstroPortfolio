307c34db7a441052a0e1abd5b5d5991e73a20b7a
Fenton <Fentontreasure@gmail.com>
2022-10-19 19:48:25 +0100

    🐛 SubProjects folder icon
    icon not showing, fix attempt #3

--- src/Components/Atoms/Icons/FolderIcon.tsx
+++ src/Components/Atoms/Icons/FolderIcon.tsx
@@ -1,6 +1,8 @@
 export default function FolderIcon() {
   return (
     <svg
+      width="24"
+      height="20"
       viewBox="0 0 32 28"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
