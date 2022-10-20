505ab5da8f92da9b0baaa24915a8af516ae45742
Fenton <Fentontreasure@gmail.com>
2022-10-20 21:35:55 +0100

    ✨ NavModal Improvement
    scrollbar removed when nav modal is displayed

--- src/Components/Atoms/BurgerMenu/BurgerMenu.tsx
+++ src/Components/Atoms/BurgerMenu/BurgerMenu.tsx
@@ -2,13 +2,24 @@ import "./BurgerMenu.scss";
 import useStore from "@store/useStore"
 import DotsNav from "@components/Molecules/DotsNav/DotsNav";
 
-export default function BurgerMenu(){
+export default function BurgerMenu() {
   const toggleBurgerMenu = useStore(state => state.toggleBurgerMenu)
   const burgerMenuToggled = useStore(state => state.burgerMenuToggled)
+
+  const handleBurgerMenu = () => {
+    const body = document.querySelector("body")
+    if (burgerMenuToggled === false) {
+      body.classList.add("body-overflow")
+    }
+    if (burgerMenuToggled === true) {
+      body.classList.remove("body-overflow")
+    }
+    toggleBurgerMenu()
+  }
   return (
     <>
       <div className="burger-menu">
-        <div className="burger-menu__line-container" onClick={toggleBurgerMenu}>
+        <div className="burger-menu__line-container" onClick={handleBurgerMenu}>
           <div className="burger-menu__lines-wrapper" >
             {[1, 2, 3].map((_, index) => (
               <span className={`burger-menu__line ${burgerMenuToggled && "line-active"}`} key={`burger-menu__line-${index}`} />
